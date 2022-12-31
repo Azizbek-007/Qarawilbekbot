5453d641060b74ec4465e5d2d7b339765e889151
Azizbek Berdimuratov <77209269+Azizbek-007@users.noreply.github.com>
2022-12-31 11:35:51 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ import middlewares, filters, handlers
 from utils.notify_admins import on_startup_notify
 from utils.set_bot_commands import set_default_commands
 
-
 async def on_startup(dispatcher):
     await set_default_commands(dispatcher)
 
